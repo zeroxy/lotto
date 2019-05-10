d1b63bb5e424da4d46129724d7dd4ff194f56d9b
zeroxy <zeroxy@users.noreply.github.com>
2019-05-10 12:40:45 +0900

    edit for frontend
    remove unused function
    change to print times

--- flasksample.py
+++ flasksample.py
@@ -11,37 +11,16 @@ def get_the_luck():
     def aug_pb(pb, pb_pow=3):
         pb=pb**pb_pow
         pb=pb/np.sum(pb)
-        #pb = pb + (1/100)
-        #pb = pb / np.sum(pb)
         return pb
-
-    def correct_test(i, pb_power=1, testtime = 70000):
-        starttime = dt.now()
-        pb = lottos.get_probability(i)
-        pb = aug_pb(pb, pb_pow=pb_power)
-        y_ = lottos.get_real_history(i)
-        count_pb = np.bincount(np.sum(np.isin( lottos.recommend(prob=pb, count=testtime),y_), axis=1), minlength=7)
-        count_no = np.bincount(np.sum(np.isin( lottos.recommend(count=testtime),y_)         , axis=1), minlength=7)
-        #(np.sort(np.argsort(-pb)[(y_-1)])+1)}\
-        ''''''
-        print(f"\n========  {i} : {(y_)} ========\
-        \npred : {count_pb}\nunif : {count_no}\nrate : {(count_pb/count_no)[:-1]}\
-        \npredict_acc : {count_pb/testtime}\npred_cumsum : {np.cumsum(count_pb/testtime)}\
-        \nuniform_acc : {count_no/testtime}\nunif_cumsum : {np.cumsum(count_no/testtime)}\
-        \n{dt.now()-starttime}\n")
-        
-        pb_correct_rate = np.sum(count_pb[3:])/np.sum(count_pb[:3])
-        no_correct_rate = np.sum(count_no[3:])/np.sum(count_no[:3])
-        return (count_pb / testtime)
-
 
     lottos = Lottos()
 
     pb, seed = lottos.get_probability()
-    r= lottos.recommend(pb)
+    r= lottos.recommend(pb, 10)
     result_obj={}
     result_obj = {
-        'recent_correct':lottos.get_real_history(len(lottos)).tolist()
+        'recent_correct':lottos.get_real_history(len(lottos)).tolist(),
+		'recent_time' : len(lottos)
     }
     result_obj['uniform'] = {
             'recommend':r.astype(np.int16).tolist(),
@@ -59,9 +38,8 @@ def get_the_luck():
     result_txt +="\n\n===================\n\n"
 
     pb, seed = lottos.get_probability()
-    r= lottos.recommend(pb)
-    pb = aug_pb(pb)
-    r= lottos.recommend(pb)
+    pb = aug_pb(pb,3)
+    r= lottos.recommend(pb, 10)
     result_obj['triple'] = {
             'recommend':r.astype(np.int16).tolist(),
             'seed':seed.tolist(),
@@ -75,9 +53,8 @@ def get_the_luck():
     result_txt +="\n\n===================\n\n"
 
     pb, seed = lottos.get_probability()
-    r= lottos.recommend(pb)
     pb = aug_pb(pb,4)
-    r= lottos.recommend(pb)
+    r= lottos.recommend(pb, 10)
     result_obj['quad'] = {
             'recommend':r.astype(np.int16).tolist(),
             'seed':seed.tolist(),
